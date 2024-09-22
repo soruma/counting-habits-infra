28a2ba9027b36aa8bdaeed1247520bfeb6afb53f
soruma <soruma.net@gmail.com>
2024-09-22 16:26:18 +0900

    cdk init

---
+++ test/counting_habits_infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CountingHabitsInfra from '../lib/counting_habits_infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/counting_habits_infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CountingHabitsInfra.CountingHabitsInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
